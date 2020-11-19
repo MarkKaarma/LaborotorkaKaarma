059bafcfb0abd79f7bd7c644cdd6fa494a7e8506
Opilane TTHK <opilane@tthk.ee>
2020-11-19 10:45:40 +0200

    Add project files.

---
+++ LaborotorkaKaarma/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LaborotorkaKaarma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("OpenSymbol", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(112, 478);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вид";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("OpenSymbol", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(293, 478);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Форма";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("OpenSymbol", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(482, 478);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Информация";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("OpenSymbol", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(782, 478);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 35);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Lavender;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(906, 438);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 591);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LaborotorkaKaarma/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaborotorkaKaarma
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent(); 
+        
+        }
+        
+        Rectangle Rectangle = new Rectangle(10, 10, 200, 100);
+        Rectangle Circle = new Rectangle(220, 10, 50, 50);
+        Rectangle Circle2 = new Rectangle(290, 10, 50, 50);
+        Rectangle Circle3 = new Rectangle(360, 10, 50, 50);
+        Rectangle Square = new Rectangle(450, 10, 150, 150);
+        
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Blue, Circle);
+            e.Graphics.FillEllipse(Brushes.Blue, Circle2);
+            e.Graphics.FillEllipse(Brushes.Blue, Circle3);
+            e.Graphics.FillRectangle(Brushes.Yellow, Square);
+            e.Graphics.FillRectangle(Brushes.Red, Rectangle);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.X < Rectangle.X + Rectangle.Width ) && (e.X > Rectangle.X))
+            {
+                if ((e.Y < Rectangle.Y + Rectangle.Height) && (e.Y > Rectangle.Y))
+                {
+                    Rectangle.Clicked = true;
+
+                    Rectangle.X = e.X - Rectangle.X;
+                    Rectangle.Y = e.Y - Rectangle.Y;
+                }
+            }
+            if ((e.X < Circle.X + Circle.Width) && (e.X > Circle.X))
+            {
+                if ((e.Y < Circle.Y + Circle.Height) && (e.Y > Circle.Y))
+                {
+                    Circle.Clicked = true;
+
+                    Circle.X = e.X - Circle.X;
+                    Circle.Y = e.Y - Circle.Y;
+                }
+            }
+            if ((e.X < Circle2.X + Circle2.Width) && (e.X > Circle2.X))
+            {
+                if ((e.Y < Circle2.Y + Circle2.Height) && (e.Y > Circle2.Y))
+                {
+                    Circle2.Clicked = true;
+
+                    Circle2.X = e.X - Circle2.X;
+                    Circle2.Y = e.Y - Circle2.Y;
+                }
+            }
+            if ((e.X < Circle3.X + Circle3.Width) && (e.X > Circle3.X))
+            {
+                if ((e.Y < Circle3.Y + Circle3.Height) && (e.Y > Circle3.Y))
+                {
+                    Circle3.Clicked = true;
+
+                    Circle3.X = e.X - Circle3.X;
+                    Circle3.Y = e.Y - Circle3.Y;
+                }
+            }
+            if ((e.X < Square.X + Square.Width) && (e.X > Square.X))
+            {
+                if ((e.Y < Square.Y + Square.Height) && (e.Y > Square.Y))
+                {
+                    Square.Clicked = true;
+
+                    Square.X = e.X - Square.X;
+                    Square.Y = e.Y - Square.Y;
+                }
+            }
+        }
+    }
+
+
+}
